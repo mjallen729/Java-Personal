7727639350915d52ed820d6009f5ac05478e1e8b
Matt <lumpychum@gmail.com>
2021-04-07 16:16:18 -0400

    Update BankClient.java

--- BankClient.java
+++ BankClient.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Diagnostic {
+class BankClient {
 	
 	public static void main(String[] args) throws IOException {
 		String username = null;
